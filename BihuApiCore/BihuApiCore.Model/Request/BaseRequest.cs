7dcb402e8af28b1ed952aed31b6adb0f2e5656c1
祁新博 <qixinbo@91bihu.com>
2018-08-07 19:20:46 +0800

    基本请求模型

---
+++ BihuApiCore/BihuApiCore.Model/Request/BaseRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BihuApiCore.Model.Request
+{
+    public class BaseRequest
+    {
+        /// <summary>
+        /// 用户Id
+        /// </summary>
+        [Required,Range(1, Int64.MaxValue, ErrorMessage = "UserID超出范围")] //刘振龙6月9号注释 前端需要联调
+        public Int64 UserId { get; set; }
+    }
+}
