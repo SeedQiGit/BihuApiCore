cdbb2d2f088d5eb9e12689a69dd9bda6619b66c6
祁新博 <seedqi@126.com>
2019-06-10 13:33:32 +0800

    修改连接字符串使用连接池

--- BihuApiCore/BihuApiCore.Model/Request/BaseRequest.cs
+++ BihuApiCore/BihuApiCore.Model/Request/BaseRequest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
 
 namespace BihuApiCore.Model.Request
 {
@@ -10,5 +13,23 @@ namespace BihuApiCore.Model.Request
         /// </summary>
         [Required,Range(1, Int64.MaxValue, ErrorMessage = "UserID超出范围")]
         public Int64 UserId { get; set; }
+    }
+    public static class BaseRequestExtension
+    {
+        /// <summary>
+        /// 根据claims获取请求基类参数
+        /// </summary>
+        /// <param name="claims"></param>
+        /// <param name="request"></param>
+        public static void GetBaseRequest(this IEnumerable<Claim>  claims,BaseRequest request)
+        {
+            var claimList = claims.ToList();
+            var employeeIdClaim =claimList.FirstOrDefault(c => c.Type == "UserId");
+            if (employeeIdClaim==null)
+            { 
+                throw new Exception("claim信息有误");
+            }
+            request.UserId = Convert.ToInt64(employeeIdClaim.Value);
+        }
     }
 }
