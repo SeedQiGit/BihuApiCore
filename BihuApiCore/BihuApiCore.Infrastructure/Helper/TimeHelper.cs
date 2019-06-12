d84d1e2d1991c28018fc4a20033908390a1b431d
祁新博 <seedqi@126.com>
2019-06-12 16:28:39 +0800

    日期扩展

---
+++ BihuApiCore/BihuApiCore.Infrastructure/Helper/TimeHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BihuApiCore.Infrastructure.Helper
+{
+    public static class TimeHelper
+    {
+        /// <summary> 
+        /// 获取时间戳 
+        /// </summary> 
+        /// <returns></returns> 
+        public static string GetTimeStamp() 
+        { 
+            TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0); 
+            return Convert.ToInt64(ts.TotalSeconds).ToString(); 
+        } 
+
+        /// <summary>
+        /// 时间戳转为C#格式时间
+        /// </summary>
+        /// <param name="timeStamp"></param>
+        /// <returns></returns>
+        public static DateTime StampToDateTime(string timeStamp)
+        {
+            timeStamp = timeStamp.Substring(0, 10);
+            double timestamp = Convert.ToInt64(timeStamp);
+            System.DateTime dateTime = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+            dateTime = dateTime.AddSeconds(timestamp).ToLocalTime();       
+            return dateTime;
+        }
+
+        /// <summary>
+        /// 获取当前时间的字符串标准格式  yyyy-MM-dd HH:mm:ss
+        /// </summary>
+        /// <returns></returns>
+        public static string NowTimeStr()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+    }
+}
