e15fda1c5225391dffd6ff82649e676d223a851a
祁新博 <seedqi@126.com>
2019-06-21 18:39:50 +0800

    加密

--- BihuApiCore/BihuApiCore.Infrastructure/Bihu/SecretCode.cs
+++ BihuApiCore/BihuApiCore.Infrastructure/Bihu/SecretCode.cs
@@ -11,7 +11,8 @@ namespace BihuApiCore.Infrastructure.Bihu
             var str = "123456&1231231";
             if (str.GetUrl().GetMd5() == str)
             {
-
+                //KeyCode是一个字符串密钥
+                var strUrl = string.Format("Name={0}&Pwd={1}&CustKey={2}&KeyCode={3}", "", "", "","asdasd");
             }
         }
     }
@@ -19,6 +20,11 @@ namespace BihuApiCore.Infrastructure.Bihu
 
     public static class SecretCodeHelper
     {
+        /// <summary>
+        /// 这里可以写个扩展，把所有属性自动过滤掉SecretCode属性。
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
         public static string GetUrl(this string url)
         {
             string[] arr = url.Split(new[] { "&" }, StringSplitOptions.RemoveEmptyEntries);
