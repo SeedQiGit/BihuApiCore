3d0cfce5b6a039545e4868089fb4478a5866d792
祁新博 <seedqi@126.com>
2019-07-10 18:51:32 +0800

    新增位枚举

---
+++ BihuApiCore/BihuApiCore.Model.UnitTest/BxShopFlagsEnumTest.cs
@@ -0,0 +1,31 @@
+﻿using BihuApiCore.Model.Enums;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace BihuApiCore.Model.UnitTest
+{
+    public class BxShopFlagsEnumTest
+    {
+        protected readonly ITestOutputHelper Output;
+
+        public BxShopFlagsEnumTest(ITestOutputHelper tempOutput)
+        {
+            Output = tempOutput;
+        }
+        [Fact]
+        public void FlagsEnumTest()
+        {
+            BxShopFlagsEnum flags = BxShopFlagsEnum.显示微信 | BxShopFlagsEnum.显示车险 | BxShopFlagsEnum.非车商城h5;
+            Output.WriteLine(flags.ToString() + "。对应数值" + (int)flags);
+            Output.WriteLine("通过数字字符串转换");
+            flags = (BxShopFlagsEnum)Enum.Parse(typeof(BxShopFlagsEnum), "5");
+            Output.WriteLine(flags.ToString() + "。对应数值" + (int)flags);
+            flags ^= BxShopFlagsEnum.非车商城h5;
+            Output.WriteLine(flags.ToString() + "。对应数值" + (int)flags);
+
+            //sql查看是否包含 SELECT * FROM `bx_agent` where (bxShop & 1) > 0
+            Assert.True((flags & BxShopFlagsEnum.显示微信) > 0);
+        }
+    }
+}
---
+++ BihuApiCore/BihuApiCore.Model/Enums/BxShopFlagsEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BihuApiCore.Model.Enums
+{
+    /// <summary>
+    /// H5移动端展示权限
+    /// </summary>
+    [Flags]
+    public enum BxShopFlagsEnum
+
+    {
+        无=0,// 0000 0000  0
+        非车商城h5 = 1 << 0,//  0000 0001  1
+        显示车险 = 1 << 1, //   0000 0010  2
+        显示微信=1 << 2,  //  0000 0100  4
+    }
+}
