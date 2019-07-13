875a3605257e6f780c8c7e1a3ebadb9b934d17d8
祁新博 <seedqi@126.com>
2019-07-13 15:20:07 +0800

    修改host

--- BihuApiCore/BihuApiCore/Program.cs
+++ BihuApiCore/BihuApiCore/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore;
+﻿using BihuApiCore.Infrastructure.Configuration;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace BihuApiCore
@@ -12,6 +13,7 @@ namespace BihuApiCore
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls(ConfigurationManager.GetValue("HostUrl").Split(','));
     }
 }
