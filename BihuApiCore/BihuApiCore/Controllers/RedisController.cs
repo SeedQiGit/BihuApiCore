db5035878e2577cccfcfac67007f3b3e39e3057d
祁新博 <seedqi@126.com>
2019-08-12 08:47:53 +0800

    redis锁

--- BihuApiCore/BihuApiCore/Controllers/RedisController.cs
+++ BihuApiCore/BihuApiCore/Controllers/RedisController.cs
@@ -17,7 +17,7 @@ namespace BihuApiCore.Controllers
         }
 
         /// <summary>
-        /// 测试get 同步
+        /// 测试
         /// </summary>
         /// <returns></returns>
         [HttpGet]
@@ -30,5 +30,46 @@ namespace BihuApiCore.Controllers
             var captchaValue = (await _database.StringGetAsync(key)).ToString();
             return BaseResponse.Ok(captchaValue);
         }
+
+        #region 锁的使用
+
+        /// <summary>
+        /// 使用redis作为并发锁
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<BaseResponse> RedisConcurrentLock()
+        {
+            string key = "test_lock1122112312311111";//如果把这个key当作锁，那么对应的token 就是他的值
+            string token = DateTime.Now.ToLongTimeString() + Guid.NewGuid().ToString();
+            //await _database.StringSetAsync(key, value, TimeSpan.FromMinutes(5));
+
+            var captchaValue = (await _database.StringGetAsync(key)).ToString();
+
+            //获取锁的token  这里应该是没有的
+            var userInfoLockObj1 = (await _database.LockQueryAsync(key));
+
+            //string token = "12312";
+
+            bool lockResult = await _database.LockTakeAsync(key, token, TimeSpan.FromSeconds(300));
+
+            //获取锁的token  这里应该是12312
+            var userInfoLockObj2 = (await _database.LockQueryAsync(key));
+            
+            //await _database.StringSetAsync(key, 1, TimeSpan.FromMinutes(5));
+            //上一行代码如果修改了锁的值，也就是key对应的value ，那么久无法释放锁
+            bool releaselockResult = await _database.LockReleaseAsync(key, token);
+
+            //获取锁的token  这里应该是没有的
+            var userInfoLockObj3 = await _database.LockQueryAsync(key);
+
+            return BaseResponse.Ok();
+        }
+        
+
+        #endregion
+
+
+
     }
 }
