b192ec1dcf2fd761b9614b3e943785dcdf351185
xinbo qi <seedqi@126.com>
2019-11-20 16:55:47 +0800

    二维码

--- BihuApiCore/BihuApiCore/Controllers/AsyncIoController.cs
+++ BihuApiCore/BihuApiCore/Controllers/AsyncIoController.cs
@@ -1,7 +1,14 @@
-﻿using BihuApiCore.Model.Response;
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using BihuApiCore.Model.Response;
 using BihuApiCore.Service.Interfaces;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using ThoughtWorks.QRCode.Codec;
 
 namespace BihuApiCore.Controllers
 {
@@ -16,6 +23,43 @@ namespace BihuApiCore.Controllers
         {
             _asyncIoService = asyncIoService;
         }
+
+        /// <summary>
+        /// 传入连接字符串 获取对应的二维码图片Base64
+        /// </summary>
+        /// <returns></returns>
+        [ProducesResponseType(typeof(BaseResponse), 1)]
+        [AllowAnonymous]
+        [HttpGet("WeiChatGetQRCodeBase64")]
+        public async Task<BaseResponse> WeiChatGetQRCodeBase64([FromQuery]string content)
+        {
+            //string content="ASDASDSA";
+            QRCodeEncoder qrEncoder = new QRCodeEncoder();
+            //二维码类型
+            qrEncoder.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE;
+            //二维码尺寸
+            qrEncoder.QRCodeScale = 4;
+            //二维码版本
+            qrEncoder.QRCodeVersion = 7;
+            //二维码容错程度
+            qrEncoder.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.M;
+            //字体与背景颜色
+            qrEncoder.QRCodeBackgroundColor = Color.White;
+            qrEncoder.QRCodeForegroundColor = Color.Black;
+            //UTF-8编码类型
+            Bitmap qrcode = qrEncoder.Encode(content, Encoding.UTF8);
+
+            //保存图片,指定保存 格式为Jpeg，占用空间会比较小
+            //qrcode.Save("D:\\temp1.jpg",ImageFormat.Jpeg);
+            //qrcode.Dispose();
+            //保存图片数据    
+            MemoryStream stream = new MemoryStream();
+            qrcode.Save(stream, ImageFormat.Jpeg);
+        
+            return BaseResponse.Ok(Convert.ToBase64String(stream.ToArray()));
+        }
+
+
 
         /// <summary>
         /// 同步方法
