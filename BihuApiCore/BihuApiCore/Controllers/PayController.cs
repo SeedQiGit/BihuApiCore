59a18202165aa2e88f2c45452497c7d6c943f61a
xinbo qi <seedqi@126.com>
2019-11-26 22:35:10 +0800

    修改插入

--- BihuApiCore/BihuApiCore/Controllers/PayController.cs
+++ BihuApiCore/BihuApiCore/Controllers/PayController.cs
@@ -5,11 +5,20 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using BihuApiCore.Model.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace BihuApiCore.Controllers
 {
     public class PayController : BaseController
     {
+        private readonly ILogger<PayController> _logger;
+
+        public PayController( ILogger<PayController> logger)
+        {
+            _logger = logger;
+        }
 
         #region 短信支付跳转页面
 
@@ -30,6 +39,10 @@ namespace BihuApiCore.Controllers
             string out_trade_no = "2019091002111158";
             //订单名称
             string subject = "短信充值";
+            //支付宝回传参数  
+            //公用回传参数，如果请求时传递了该参数，则返回给商户时会回传该参数。支付宝只会在同步返回（包括跳转回商户网站）和异步通知时将该参数原样返回。本参数必须进行UrlEncode之后才可以发送给支付宝。
+            //不过好像不好用，最后我还是用body了。。。。
+            string passback_params= "用户id1231";
             //必填 
             string total_fee = "0.01";
             //订单描述       
@@ -54,6 +67,7 @@ namespace BihuApiCore.Controllers
             sParaTemp.Add("seller_email", seller_email);
             sParaTemp.Add("out_trade_no", out_trade_no);
             sParaTemp.Add("subject", subject);
+            sParaTemp.Add("passback_params", passback_params);
             sParaTemp.Add("total_fee", total_fee);
             sParaTemp.Add("body", body);
             sParaTemp.Add("show_url", show_url);
@@ -74,6 +88,151 @@ namespace BihuApiCore.Controllers
         }
 
         #endregion
+           #region 短信支付后回调地址
+
+        /// <summary>
+        /// 短信支付后回调地址 ReturnUrl
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult> MessagePayReturnUrl()
+        {
+            _logger.LogInformation("接收到短信支付后回调;" );
+            SortedDictionary<string, string> sPara = GetRequestGet();
+            if (sPara.Count > 0)//判断是否有带返回参数
+            {
+                Notify aliNotify = new Notify();
+                bool verifyResult = aliNotify.Verify(sPara, Request.Query["notify_id"], Request.Query["sign"]);
+                string content = string.Format("交易号：{0}^订单号：{1}^交易状态：{2}^交易金额：{3}", Request.Query["trade_no"], Request.Query["out_trade_no"], Request.Query["trade_status"], Request.Query["total_fee"]);
+                _logger.LogInformation("同步回调：" + content);
+                if (verifyResult)//验证成功
+                {
+                    //订单号
+                    string out_trade_no = Request.Query["out_trade_no"];
+                    //支付宝交易号
+                    string trade_no = Request.Query["trade_no"];
+
+                    //交易状态
+                    string trade_status = Request.Query["trade_status"];
+
+                    string subject= Request.Query["body"];
+                
+                    if (Request.Query["trade_status"] == "TRADE_FINISHED" || Request.Query["trade_status"] == "TRADE_SUCCESS")
+                    {
+
+                        #region 调用支付成功服务  是不是在通知里面调用会更好？？   都调用以下吧，做成幂等的 加锁  但是影响反应速度啊！！！
+
+#pragma warning disable 4014
+                        
+#pragma warning restore 4014
+                        #endregion
+
+                        return Redirect(ConfigurationManager.GetValue("ForeEndUrl")+"/#/marketManage/PaySuccess?orderNum="+out_trade_no);
+                    }
+                }
+            }
+            return Redirect(ConfigurationManager.GetValue("ForeEndUrl")+"/#/marketManage/PayFail");
+          
+        }
+
+        /// <summary>
+        /// 获取支付宝GET过来通知消息，并以“参数名=参数值”的形式组成数组
+        /// </summary>
+        /// <returns>request回来的信息组成的数组</returns>
+        private SortedDictionary<string, string> GetRequestGet()
+        {
+            SortedDictionary<string, string> sArray = new SortedDictionary<string, string>();
+            IQueryCollection coll;
+            //Load Form variables into NameValueCollection variable.
+            coll = Request.Query;
+           
+            foreach (var item in coll)
+            {
+                sArray.Add(item.Key, item.Value);
+            }
+
+            return sArray;
+        }
+
+        #endregion
+
+        #region 短信支付后阿里通知地址
+
+        /// <summary>
+        ///  短信支付后阿里通知地址
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<BaseResponse> MessagePayNotify()
+        {
+            _logger.LogInformation("收到短信支付后通知" );
+            SortedDictionary<string, string> sPara = GetRequestPost();
+
+            if (sPara.Count > 0)//判断是否有带返回参数
+            {
+                Notify aliNotify = new Notify();
+                bool verifyResult = aliNotify.Verify(sPara, Request.Form["notify_id"], Request.Form["sign"]);
+                string content = string.Format("交易号:{0}^订单号:{1}^交易状态:{2}^交易金额:{3}", Request.Form["trade_no"], Request.Form["out_trade_no"], Request.Form["trade_status"], Request.Form["total_fee"]);
+                _logger.LogInformation("异步回调：" + content);
+                if (verifyResult)//验证成功
+                {
+                    //商户订单号
+                    string out_trade_no = Request.Form["out_trade_no"];
+
+                    //支付宝交易号
+                    string trade_no = Request.Form["trade_no"];
+
+                    string subject= Request.Query["body"];
+
+                    //交易状态
+                    string trade_status = Request.Form["trade_status"];
+                    if (Request.Form["trade_status"] == "TRADE_SUCCESS" || Request.Form["trade_status"] == "TRADE_FINISHED")
+                    {
+                        //判断该笔订单是否在商户网站中已经做过处理
+                        //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+                        //如果有做过处理，不执行商户的业务程序
+
+                        //注意：
+                        //该种交易状态只在一种情况下出现——开通了高级即时到账，买家付款成功后。
+
+#pragma warning disable 4014
+                        
+#pragma warning restore 4014
+                    }
+                    return BaseResponse.Ok();
+                   
+                }
+                else//验证失败
+                {
+                    return BaseResponse.Failed();
+                }
+            }
+            return BaseResponse.Failed("无通知参数");
+        }
+
+        /// <summary>
+        /// 获取支付宝POST过来通知消息，并以“参数名=参数值”的形式组成数组
+        /// </summary>
+        /// <returns>request回来的信息组成的数组</returns>
+        private SortedDictionary<string, string> GetRequestPost()
+        {
+            SortedDictionary<string, string> sArray = new SortedDictionary<string, string>();
+       
+            var coll = Request.Form;
+           
+            foreach (var item in coll)
+            {
+                sArray.Add(item.Key, item.Value);
+            }
+
+            return sArray;
+        }
+
+        #endregion
+
+
+
+
 
     }
 }
