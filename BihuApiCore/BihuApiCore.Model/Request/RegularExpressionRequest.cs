cd05509bb4cdadf5573298f74a46904436363290
xinbo qi <seedqi@126.com>
2020-03-15 20:47:32 +0800

    code first迁移

---
+++ BihuApiCore/BihuApiCore.Model/Request/RegularExpressionRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BihuApiCore.Model.Request
+{
+    public class RegularExpressionRequest
+    {
+        [RegularExpression(@"^1[^012]\d{9}$", ErrorMessage = "请输入正确的手机！")]
+        public string Mobile { get; set; }
+    }
+}
---
+++ BihuApiCore/BihuApiCore/Controllers/RegularExpressionController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using BihuApiCore.Filters;
+using BihuApiCore.Model.Request;
+using BihuApiCore.Model.Response;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BihuApiCore.Controllers
+{
+    public class RegularExpressionController:BaseController
+    {
+        private readonly ILogger<RegularExpressionController> _logger;
+
+        public RegularExpressionController( ILogger<RegularExpressionController> _logger )
+        {
+            _logger = _logger;
+        }
+
+        [HttpPost]
+        //[ModelVerifyFilter] 这里用全局的
+        public async Task<BaseResponse> TestPost([FromBody]RegularExpressionRequest request)
+        {
+            
+            return BaseResponse.Ok();
+        }
+
+
+
+    }
+}
